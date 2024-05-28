513eb712c1c0f4bc466251b26ba9353b4f1f1ec1
KSV <ksv@d.c>
2024-05-28 23:52:51 +0300

    fix: router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 
 import HomePage from './pages/HomePage.vue'
 import FavoritiesPage from './pages/FavoritiesPage.vue'
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes
 })
 
