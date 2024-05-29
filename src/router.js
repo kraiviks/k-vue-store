5b715140e760a570e73cd3664e34c2f6a408fbec
KSV <ksv@d.c>
2024-05-29 19:10:53 +0300

    feature: dynamic title

--- src/router.js
+++ src/router.js
@@ -4,13 +4,29 @@ import HomePage from './pages/HomePage.vue'
 import FavoritiesPage from './pages/FavoritiesPage.vue'
 
 const routes = [
-  { path: '/', component: HomePage },
-  { path: '/favorities', component: FavoritiesPage }
+  {
+    path: '/',
+    component: HomePage,
+    meta: {
+      title: 'Vue Store'
+    }
+  },
+  {
+    path: '/favorities',
+    component: FavoritiesPage,
+    meta: {
+      title: 'Vue Store | Favorities'
+    }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to) => {
+  document.title = to.meta?.title ?? 'Default Title'
 })
 
 export default router
