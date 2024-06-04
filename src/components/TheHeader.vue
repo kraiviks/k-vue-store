0e69cc7004cef5ddc8915bf249f648f31e96df37
kraiviks <kraiviks@gmail.com>
2024-06-04 18:35:59 +0300

    update: add animation on active cart

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -26,15 +26,23 @@ const { handlerOpenDrawer } = store
           class="flex items-center gap-3 cursor-pointer hover:text-black transition"
           @click="handlerOpenDrawer"
         >
-          <img src="/cart.svg" alt="Cart" />
-          <b>{{ cart.totalPrice > 0 ? cart.totalPrice.toFixed(2) : 0 }}$</b>
+          <img
+            src="/cart.svg"
+            alt="Cart"
+            :class="cart.totalPrice.toFixed(1) > 0 && 'animateCart'"
+          />
+          <span>{{ cart.totalPrice > 0 ? cart.totalPrice.toFixed(2) : 0 }}$</span>
         </li>
         <li
-          :class="favorities.length > 0 ? 'text-black font-bold' : 'text-slate-400'"
+          :class="favorities.length > 0 ? 'text-black' : 'text-slate-400'"
           class="flex items-center gap-3 cursor-pointer hover:text-black transition"
         >
           <RouterLink to="/favorities" class="flex items-center gap-3">
-            <img src="/heart.svg" alt="heart" />
+            <img
+              src="/heart.svg"
+              alt="heart"
+              :class="favorities.length > 0 && 'animateFavorities'"
+            />
             <span>Закладки</span>
           </RouterLink>
         </li>
@@ -42,3 +50,32 @@ const { handlerOpenDrawer } = store
     </nav>
   </header>
 </template>
+
+<style>
+.animateCart {
+  animation: animateCart 0.7s ease infinite alternate-reverse;
+}
+@keyframes animateCart {
+  0% {
+    transform: rotate(-10deg);
+  }
+
+  50% {
+    transform: rotate(0deg);
+  }
+  100% {
+    filter: drop-shadow(1px 1px 2px grey);
+    transform: scale(1.2) rotate(10deg);
+  }
+}
+
+.animateFavorities {
+  animation: animateFavorities 1s ease infinite alternate-reverse;
+}
+@keyframes animateFavorities {
+  to {
+    filter: drop-shadow(1px 1px 2px grey);
+    transform: scale(1.2);
+  }
+}
+</style>
